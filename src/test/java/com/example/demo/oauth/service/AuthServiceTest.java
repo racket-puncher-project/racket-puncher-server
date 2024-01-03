5982805e47744f7c997a143e91ac2790fd19662b
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-01-03 13:31:31 +0900

    refactor: 로직 분리에 따른 테스트 코드 수정

--- src/test/java/com/example/demo/oauth/service/AuthServiceTest.java
+++ src/test/java/com/example/demo/oauth/service/AuthServiceTest.java
@@ -3,23 +3,33 @@ package com.example.demo.oauth.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import com.example.demo.entity.SiteUser;
 import com.example.demo.exception.RacketPuncherException;
+import com.example.demo.oauth.dto.AccessTokenDto;
 import com.example.demo.oauth.dto.SignUpDto;
 import com.example.demo.siteuser.repository.SiteUserRepository;
 import com.example.demo.type.AgeGroup;
 import com.example.demo.type.GenderType;
 import com.example.demo.type.Ntrp;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @ExtendWith(MockitoExtension.class)
 class AuthServiceTest {
@@ -77,4 +87,31 @@ class AuthServiceTest {
         assertEquals(exception.getMessage(), "이미 사용 중인 이메일입니다.");
     }
 
+//    @Test
+//    public void testReissue() throws Exception {
+//        // given
+//        String accessToken = "accessToken";
+//        String refreshToken = "refreshToken";
+//        String newAccessToken = "newAccessToken";
+//        String newRefreshToken = "newRefreshToken";
+//        String username = "username";
+//        AccessTokenDto accessTokenDto = new AccessTokenDto(accessToken);
+//        Authentication authentication = mock(Authentication.class);
+//        ValueOperations<String, String> valueOperations = mock(ValueOperations.class);
+//
+//        when(authentication.getName()).thenReturn(username);
+//        when(tokenProvider.getAuthentication(accessToken)).thenReturn(authentication);
+//        when(redisTemplate.opsForValue()).thenReturn(valueOperations);
+//        when(redisTemplate.opsForValue().get(authentication.getName())).thenReturn(refreshToken);
+//        when(tokenProvider.generateAccessToken(username)).thenReturn(newAccessToken);
+//        when(redisTemplate.delete(authentication.getName())).thenReturn(null);
+//        when(tokenProvider.generateAndSaveRefreshToken(authentication.getName())).thenReturn(newRefreshToken);
+//
+//        // when
+//        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/reissue")
+//                        .contentType(MediaType.APPLICATION_JSON)
+//                        .content(new ObjectMapper().writeValueAsString(accessTokenDto)))
+//                .andExpect(MockMvcResultMatchers.status().isOk())
+//                .andDo(print());
+//    }
 }
