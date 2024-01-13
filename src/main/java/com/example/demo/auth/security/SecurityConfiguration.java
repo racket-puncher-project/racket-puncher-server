ba9473dc8437f03ccf1d3a42bf27e6baefd0111a
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-01-13 21:07:01 +0900

    fix: 주소 api 로그인 없이 사용 가능하도록 설정

--- src/main/java/com/example/demo/auth/security/SecurityConfiguration.java
+++ src/main/java/com/example/demo/auth/security/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration {
                                 "/api/auth/upload-profile-image",  "/api/matches/list/**",
                                 "/api/matches/detail/**", "/api/users/profile/**", "/api/aws/**",
                                 "/api/auth/check-nickname", "/api/auth/check-email", "/api/auth/redis",
-                                "/api/auth/kakao")
+                                "/api/auth/kakao", "/api/matches/address")
 
                         .permitAll()
                         .anyRequest().authenticated())
