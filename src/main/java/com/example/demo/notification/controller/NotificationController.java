b51273d621084439e9401b3e3829afa16a8cd7e4
hyun-Jin-Park-4808 <guswlsdl4808@gmail.com>
2024-04-08 20:49:08 +0900

    fix: SSE 연결이 제대로 이뤄지지 않아 Content-Type을 추가로 지정해줍니다.

--- src/main/java/com/example/demo/notification/controller/NotificationController.java
+++ src/main/java/com/example/demo/notification/controller/NotificationController.java
@@ -7,7 +7,6 @@ import com.example.demo.notification.service.NotificationService;
 import com.example.demo.siteuser.repository.SiteUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
